1d0e30b52c3aaa46a1f15db920db48c3e1e2ee21
Kennedy777 <reach.marguerite@gmail.com>
2019-06-07 15:50:44 -0700

    add initial JS files

---
+++ src/docsearch.js
@@ -0,0 +1,13 @@
+export default class DocSearch {
+    constructor(name, picture, street, city, state, zip, phone, noResults) {
+  
+      this.name = name;
+      this.street = street;
+      this.city = city;
+      this.state = state;
+      this.zip = zip;
+      this.phone = phone;
+      this.noResults = noResults;
+    }
+  }
+  
