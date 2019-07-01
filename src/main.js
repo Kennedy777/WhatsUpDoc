c2b6a557c6094a48af2e3d50a3d333f1d2cb838f
Kennedy777 <reach.marguerite@gmail.com>
2019-07-01 11:30:00 -0700

    add business logic to JS

--- src/main.js
+++ src/main.js
@@ -8,7 +8,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   $('#search-button').click(function(){
-        event.preventDefault();
+    const newDocSearch = new DocSearch;
+    const symptoms = $('#symptoms').val();
+    $('#symptoms').val('');
+    const mdFirstName = $('#doctor-first-name').val();
+    $('#doctor-first-name').val('');
+    const mdLastName = $('#doctor-last-name').val();
+    $('#doctor-last-name').val('');
+    $ajax({
+      url: 
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response){
+        newDocSearch.noResults = response.meta.total;
+        if (newDocSearch.noResults == 0) {
+          $('#results').html('<h2>Results:</h2><p>No doctors found that meet your criteria. Please try again.</p>')
+          } else {
+          newDocSearch.name = response.data[0].practices[0].name;
+          newDocSearch.picture = response.data[0].profile.image_url;
+          newDocSearch.street = response.data[0].practices[0].visit_address.street;
+          
+
+      }
+
+
+    })
+    event.preventDefault();
+
+
       });
   });
 
